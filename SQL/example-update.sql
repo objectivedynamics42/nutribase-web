b76ab2fdf7db32d5bb314f66957f4eedaa257323
Michael Baker <objectivedynamics@gmail.com>
2025-01-19 07:12:05 +0000

    tidy

--- SQL/example-update.sql
+++ SQL/example-update.sql
@@ -2,3 +2,8 @@ UPDATE food
    SET food.protein_grams_per_unit = 2.5,
        food.name = 'Apricot - Dried, Alesto'
    WHERE food.name LIKE '%apricot - dried%';
+
+-- Handling the aportrophe in Sainsbury's
+UPDATE food
+   SET food.name = 'Tagliatelli - Dried, Sainsbury''s'
+   WHERE food.name = 'Penne';
