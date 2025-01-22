403c81d775ebfb728e6038a3ecc6093eca5d5c27
Michael Baker <objectivedynamics@gmail.com>
2025-01-22 08:32:51 +0000

    render back links

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -1,32 +1,37 @@
 <?php
 
-function renderHeader(string $title): string {
-    return "<!-- helpers.renderHeader --><div class=\"container\">" .
-        "<div class=\"row custom-blue-bg text-white py-3 align-items-center\">" .
-            // Left empty column for spacing
-            "<div class=\"col\"></div>" .
-            // Centered heading
-            "<div class=\"col-auto text-center\">" .
-                "<h1 class=\"mb-0\">nutribase</h1>" .
-            "</div>" .
-            // Account icon on the right with same width as left column
-            "<div class=\"col d-flex justify-content-end\">" .
-                "<div class=\"dropdown\">" .
-                    "<a href=\"#\" class=\"btn btn-link text-white text-decoration-none dropdown-toggle\" " .
-                    "id=\"accountDropdown\" " .
-                    "role=\"button\" " .
-                    "data-bs-toggle=\"dropdown\" " .
-                    "aria-expanded=\"false\" " .
-                    "aria-label=\"Account\">" .
-                        "<img src=\"/images/account.svg\" alt=\"Account\" width=\"30\" height=\"30\">" .
+function renderHeader(string $backPage, string $title): string {
+    return "<!-- helpers.renderHeader -->" .
+        "<div class=\"container\">" .
+            "<div class=\"row custom-blue-bg text-white py-3 align-items-center\">" .
+                // Left empty column for spacing
+                "<div class=\"col d-flex justify-content-start\">" .
+                    "<a href=" . $backPage . " class=\"btn btn-link text-white text-decoration-none\" aria-label=\"Go back\">" .
+                      "<img src=\"/images/back-svgrepo-com.svg\" alt=\"Go back\" width=\"30\" height=\"30\">" .
                     "</a>" .
-                    "<ul class=\"dropdown-menu dropdown-menu-end\" aria-labelledby=\"accountDropdown\">" .
-                        "<li><a class=\"dropdown-item\" href=\"/login\">Log In</a></li>" .
-                        "<!-- Add more menu options here if needed -->" .
-                    "</ul>" .
+                "</div>" .
+                // Centered heading
+                "<div class=\"col-auto text-center\">" .
+                    "<h1 class=\"mb-0\">nutribase</h1>" .
+                "</div>" .
+                // Account icon on the right with same width as left column
+                "<div class=\"col d-flex justify-content-end\">" .
+                    "<div class=\"dropdown\">" .
+                        "<a href=\"#\" class=\"btn btn-link text-white text-decoration-none dropdown-toggle\" " .
+                        "id=\"accountDropdown\" " .
+                        "role=\"button\" " .
+                        "data-bs-toggle=\"dropdown\" " .
+                        "aria-expanded=\"false\" " .
+                        "aria-label=\"Account\">" .
+                            "<img src=\"/images/account.svg\" alt=\"Account\" width=\"30\" height=\"30\">" .
+                        "</a>" .
+                        "<ul class=\"dropdown-menu dropdown-menu-end\" aria-labelledby=\"accountDropdown\">" .
+                            "<li><a class=\"dropdown-item\" href=\"/login\">Log In</a></li>" .
+                            "<!-- Add more menu options here if needed -->" .
+                        "</ul>" .
+                    "</div>" .
                 "</div>" .
             "</div>" .
-        "</div>" .
         // Subheading
         "<div class=\"row bg-secondary text-white py-2\">" .
             "<div class=\"col d-flex justify-content-center\">" .
