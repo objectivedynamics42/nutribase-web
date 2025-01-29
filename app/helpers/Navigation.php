1d5eda68176f7217a9f8b732bfbd0e836ef0f1f9
Michael Baker <objectivedynamics@gmail.com>
2025-01-29 19:33:42 +0000

    extend constructor

--- app/helpers/Navigation.php
+++ app/helpers/Navigation.php
@@ -3,12 +3,18 @@
 class Navigation {
 
     private string $backlinkUrl;
+    private array $menu;
 
-    public function __construct(string $backlinkUrl) {
+    public function __construct(string $backlinkUrl, array $menu) {
         $this->backlinkUrl = $backlinkUrl;
+        $this->menu = $menu;
     }
 
     public function getBacklinkUrl() : string{
         return $this->backlinkUrl;
+    }
+
+    public function getMenu() : array {
+        return $this->menu;
     }
 }
