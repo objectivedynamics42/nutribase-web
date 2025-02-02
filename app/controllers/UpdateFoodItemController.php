16a87733734761946c7a3941bd29acabde2e3c10
Michael Baker <objectivedynamics@gmail.com>
2025-02-02 06:44:12 +0000

    added user table sql

--- app/controllers/UpdateFoodItemController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class UpdateFoodItemController {
-    public function handleUpdates() {
-        sendResponse('<html><head><title>Update Food Item</title></head><body><b>Coming Soon</b></body></html>', 'text/html');
-    }
-}
--- app/controllers/UpdateFoodsController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class UpdateFoodsController {
-
-    public function handleUpdates() {
-        sendResponse('<html><head><title>Update Foods</title></head><body><b>Coming Soon</b></body></html>', 'text/html');
-    }
-}
